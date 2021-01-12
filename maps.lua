e6f80ce948c2b93f9377c1298216d61750fac9c0
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-01-12 10:40:22 +0800

    4 new maps

--- maps.lua
+++ maps.lua
@@ -1,23 +1,25 @@
+a = fromFile()
+
 map = {}
 
-map[1] = {
-	{o = "wall", x = 250, y = 200, w = 650, h = 200},
+map[1] = {beaten = a[1],
+	{o = "wall", x = 250, y = 200, w = 550, h = 200},
 	{o = "evil", x = 700, y = 100}
 }
 
-map[2] = {
+map[2] = {beaten = a[2],
 	{o = "wall", x = 250, y = 350, w = 300, h = 250},
 	{o = "wall", x = 400, y = 0, w = 150, h = 250},
 	{o = "evil", x = 700, y = 500}
 }
 
-map[3] = {
+map[3] = {beaten = a[3],
 	{o = "wall", x = 250, y = 250, w = 300, h = 350},
 	{o = "evil", x = 700, y = 300},
 	{o = "evil", x = 700, y = 500}
 }
 
-map[4] = {
+map[4] = {beaten = a[4],
 	{o = "wall", x = 300, y = 250, w = 100, h = 350},
 	{o = "wall", x = 125, y = 250, w = 175, h = 100},
 	{o = "wall", x = 400, y = 350, w = 175, h = 100},
@@ -25,4 +27,37 @@ map[4] = {
 	{o = "evil", x = 700, y = 500},
 	{o = "evil", x = 50, y = 200},
 	{o = "evil", x = 650, y = 600}
+}
+
+map[5] = {beaten = a[5],
+	{o = "wall", x = 250, y = 0, w = 300, h = 350},
+	{o = "evil", x = 700, y = 300},
+	{o = "evil", x = 700, y = 100}
+}
+
+map[6] = {beaten = a[5],
+	{o = "wall", x = 300, y = 200, w = 200, h = 200},
+	{o = "evil", x = 700, y = 300},
+	{o = "evil", x = 700, y = 100}
+}
+
+map[7] = {beaten = a[5],
+	{o = "wall", x = 250, y = 200, w = 75, h = 400}, 
+	{o = "wall", x = 525, y = 0, w = 75, h = 400}, 
+	{o = "evil", x = 700, y = 50},
+	{o = "evil", x = 375, y = 500}
+}
+
+map[8] = {beaten = a[5],
+	{o = "wall", x = 0, y = 300, w = 300, h = 100}, 
+	{o = "wall", x = 400, y = 300, w = 100, h = 300}, 
+	{o = "evil", x = 700, y = 50},
+	{o = "evil", x = 375, y = 500}
+}
+
+map[9] = {beaten = a[5],
+	{o = "wall", x = 300, y = 0, w = 500, h = 100}, 
+	{o = "wall", x = 200, y = 300, w = 300, h = 300}, 
+	{o = "evil", x = 700, y = 200},
+	{o = "evil", x = 600, y = 500}
 }
